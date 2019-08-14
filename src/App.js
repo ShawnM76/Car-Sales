5615a258758e79263d2d361682d81f0c2661840e
Shawn McManus <xsvfan76@gmail.com>
2019-08-14 16:41:22 -0600

    passing data through app but having tons of errors

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { titleReducer } from './reducers/titleReducer';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
-export const store = createStore(state);
+export const store = createStore(titleReducer);
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: [],
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 },
-    ],
-  };
+const App = props => {
+  // const state = {
+  //   additionalPrice: 0,
+  //   car: {
+  //     price: 26395,
+  //     name: '2019 Ford Mustang',
+  //     image:
+  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+  //     features: [],
+  //   },
+  //   store: [
+  //     { id: 1, name: 'V-6 engine', price: 1500 },
+  //     { id: 2, name: 'Racing detail package', price: 1500 },
+  //     { id: 3, name: 'Premium sound system', price: 500 },
+  //     { id: 4, name: 'Rear spoiler', price: 250 },
+  //   ],
+  // };
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
@@ -39,12 +41,12 @@ const App = () => {
     <Provider store={store}>
       <div className='boxes'>
         <div className='box'>
-          <Header car={state.car} />
-          <AddedFeatures car={state.car} />
+          <Header car={props.car} />
+          <AddedFeatures car={props.car} />
         </div>
         <div className='box'>
-          <AdditionalFeatures store={state.store} />
-          <Total car={state.car} additionalPrice={state.additionalPrice} />
+          <AdditionalFeatures store={props.store} />
+          <Total car={props.car} additionalPrice={props.additionalPrice} />
         </div>
       </div>
     </Provider>
