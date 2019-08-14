c501b0670b43c22ec3afc7d050710fea429356cd
Shawn McManus <xsvfan76@gmail.com>
2019-08-14 15:33:14 -0600

    imported provider and create store and wraped app in Provider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+export const store = createStore(state);
 
 const App = () => {
   const state = {
@@ -13,14 +17,14 @@ const App = () => {
       name: '2019 Ford Mustang',
       image:
         'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
+      features: [],
     },
     store: [
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
+      { id: 4, name: 'Rear spoiler', price: 250 },
+    ],
   };
 
   const removeFeature = item => {
@@ -32,16 +36,18 @@ const App = () => {
   };
 
   return (
-    <div className="boxes">
-      <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+    <Provider store={store}>
+      <div className='boxes'>
+        <div className='box'>
+          <Header car={state.car} />
+          <AddedFeatures car={state.car} />
+        </div>
+        <div className='box'>
+          <AdditionalFeatures store={state.store} />
+          <Total car={state.car} additionalPrice={state.additionalPrice} />
+        </div>
       </div>
-      <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
-      </div>
-    </div>
+    </Provider>
   );
 };
 
